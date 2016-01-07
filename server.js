90241e8c7e352370c2b8df9658cc1b8be2cc4360
Jack Yeh <jack.yeh@gmail.com>
2016-01-06 19:01:27 -0700

    create/pass tests: page should return dymically created pizzas and page should not return 404 - fetch the pizza from the data store - render the pizza.jade template passing in the pizza from the data store - the pizza should be name-spaced so that it doesnt conflict with the applications title and what not

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ app.get('/', (request, response) => {
   response.render('index');
 });
 
+app.get('/pizzas/:id', (request, response) => {
+  var pizza = app.locals.pizzas[request.params.id];
+
+  response.render('pizza', { pizza: pizza });
+});
 
 app.post('/pizzas', (request, response) => {
   var id = generateID();
